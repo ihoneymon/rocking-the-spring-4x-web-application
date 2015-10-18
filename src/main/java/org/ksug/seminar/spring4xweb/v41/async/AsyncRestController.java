3e6fe9c87a55324548460629d4909a1ed37f65d9
honeymon <ihoneymon@gmail.com>
2015-10-18 19:03:44 +0900

    4.1에 추가된 ListenableFuture 구현체 추가

---
+++ src/main/java/org/ksug/seminar/spring4xweb/v41/async/AsyncRestController.java
@@ -0,0 +1,33 @@
+package org.ksug.seminar.spring4xweb.v41.async;
+
+import java.util.Map;
+
+import lombok.extern.slf4j.Slf4j;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.util.concurrent.ListenableFuture;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * 
+ * @author honeymon
+ *
+ */
+@Slf4j
+@RestController
+@RequestMapping("/v41")
+public class AsyncRestController {
+
+    @Autowired
+    private AsyncService asyncService;
+
+    @RequestMapping("/async/{id}")
+    public ListenableFuture<Map<String, String>> async(@PathVariable String id) throws InterruptedException {
+        log.info("start");
+        ListenableFuture<Map<String, String>> result = asyncService.execute(id);
+        log.info("end");
+        return result;
+    }
+}
---
+++ src/main/java/org/ksug/seminar/spring4xweb/v41/async/AsyncService.java
@@ -0,0 +1,32 @@
+package org.ksug.seminar.spring4xweb.v41.async;
+
+import java.util.Map;
+import java.util.concurrent.Executors;
+
+import javax.annotation.PostConstruct;
+
+import org.springframework.core.task.AsyncListenableTaskExecutor;
+import org.springframework.scheduling.concurrent.ConcurrentTaskExecutor;
+import org.springframework.stereotype.Service;
+import org.springframework.util.concurrent.ListenableFuture;
+
+import com.google.common.collect.ImmutableMap;
+
+@Service
+public class AsyncService {
+
+    private AsyncListenableTaskExecutor executorService;
+
+    @PostConstruct
+    public void setUp() {
+        executorService = new ConcurrentTaskExecutor(Executors.newFixedThreadPool(10));
+    }
+
+    public ListenableFuture<Map<String, String>> execute(String id) throws InterruptedException {
+        return executorService.submitListenable(() -> {
+            Thread.sleep(2000);
+            return ImmutableMap.of("id", id, "thread", Thread.currentThread().toString());
+        });
+    }
+
+}
