151cc17dce109410912ce6377c95209258d4752d
honeymon <ihoneymon@gmail.com>
2015-10-18 19:20:10 +0900

    Path matching 설정 추가

--- src/main/java/org/ksug/seminar/spring4xweb/configuration/WebConfiguration.java
+++ src/main/java/org/ksug/seminar/spring4xweb/configuration/WebConfiguration.java
@@ -1,9 +1,14 @@
 package org.ksug.seminar.spring4xweb.configuration;
 
 import org.springframework.boot.autoconfigure.web.WebMvcAutoConfiguration.WebMvcAutoConfigurationAdapter;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.util.AntPathMatcher;
+import org.springframework.util.PathMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.PathMatchConfigurer;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
+import org.springframework.web.util.UrlPathHelper;
 
 @Configuration
 @EnableWebMvc
@@ -17,6 +22,29 @@ public class WebConfiguration extends WebMvcAutoConfigurationAdapter {
         registry.addViewController("/error/default").setViewName("error/default");
         registry.addViewController("/error/400").setViewName("error/400");
         registry.addViewController("/error/404").setViewName("error/404");
+    }
+
+    /**
+     * Path Matching
+     */
+    @Override
+    public void configurePathMatch(PathMatchConfigurer configurer) {
+        configurer
+            .setUseSuffixPatternMatch(true)
+            .setUseTrailingSlashMatch(false)
+            .setUseRegisteredSuffixPatternMatch(true)
+            .setPathMatcher(pathMatcher())
+            .setUrlPathHelper(urlPathHelper());
+    }
+
+    @Bean
+    public UrlPathHelper urlPathHelper() {
+        return new UrlPathHelper();
+    }
+
+    @Bean
+    public PathMatcher pathMatcher() {
+        return new AntPathMatcher();
     }
 
 }
